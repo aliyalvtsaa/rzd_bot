49d9a8ae4c0b635326784bfc273cc81d5dc5c078
aliyalvtsaa <128593426+aliyalvtsaa@users.noreply.github.com>
2023-12-25 16:31:45 +0300

    Delete db_connection.py

--- db_connection.py
+++
@@ -1,9 +0,0 @@
-from mongodb_connector import MongoDBConnector
-uri = "mongodb+srv://aliyasta:2lzTBzOi5d0iplfw@cluster0.ndrzlgu.mongodb.net/?retryWrites=true&w=majority"
-db_name = "user_data"
-mongo_connector = MongoDBConnector(uri, db_name)
-all_data = mongo_connector.get_all_data()
-
-# Вывод данных
-for data in all_data:
-    print(data)
